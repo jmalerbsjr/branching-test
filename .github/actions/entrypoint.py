1b75836a2661b37cf69c24cdd0fa7cd1d58da726
Joe Malerba <joe.malerba@csgi.com>
2020-09-08 16:40:35 -0400

    Final

--- .github/actions/entrypoint.py
+++ .github/actions/entrypoint.py
@@ -1,14 +1,8 @@
 import os
-import re
-import subprocess
-import json
 from github import Github
 
-# my_input = os.environ["INPUT_MYINPUT"]
+# input = os.environ["INPUT_MYINPUT"]
 # print("::set-output name=myOutput::{0}".format(my_input))
-
-# status = "IT WORKS"
-# print("::set-output name=status::{0}".format(status))
 
 class SemVerModel(object):
 
@@ -26,18 +20,18 @@ class SemVerModel(object):
         self.tag_next = tag_next
         self.tag_meta = {"development":
                              [
-                                 {"tag_latest": None,
-                                 "tag_latest_nosuffix": None,
-                                 "tag_suffix": "-dev",
-                                 "prerelease": True
+                                 {
+                                 # "tag_suffix": "-dev",
+                                 "prerelease": True,
+                                 "prerelease_name": "Development"
                                  }
                              ],
                          "master":
                              [
-                                 {"tag_latest": None,
-                                 "tag_latest_nosuffix": None,
-                                 "tag_suffix": "",
-                                 "prerelease": False
+                                 {
+                                 # "tag_suffix": "",
+                                 "prerelease": False,
+                                 "prerelease_name": "Production Release"
                                  }
                              ]
                          }
@@ -47,13 +41,14 @@ class MhConfigModel(object):
 
     def __init__(self,
                  mode='live',
-                 src_branch=None,
                  dest_branch=None,
-                 github_api_token=None):
+                 github_api_token=None,
+                 repo_name=None):
 
         self.mode = mode
         self.dest_branch = dest_branch
         self.github_api_token = github_api_token
+        self.repo_name = repo_name
 
 
 def connect_github(api_token):
@@ -66,61 +61,19 @@ def get_github_tags(repo_name=None):
     # Get tags from GitHub as a list
     repo = gh_api.get_user().get_repo(repo_name)
     git_tags = repo.get_tags()
+
     return [tag.name for tag in git_tags]
 
 
 def get_latest_tag(git_tags):
-    # Get latest tag for each branch and set values in semver.tag_meta
     print("Find latest tags")
 
     return git_tags[0]
-    #
-    # for branch in semver.tag_meta:
-    #     # Set tag suffix for given branch
-    #     tag_suffix = semver.tag_meta[branch][0]['tag_suffix']
-    #
-    #     for tag in git_tags:
-    #         regex = re.search(r'(\d+\.\d+\.\d+){0}$'.format(tag_suffix), tag)
-    #         if regex:
-    #             # Set latest tag values with and without tag suffix
-    #             semver.tag_meta[branch][0]['tag_latest'] = tag
-    #             semver.tag_meta[branch][0]['tag_latest_nosuffix'] = regex.group(1)
-    #             print('({0}): {1}'.format(branch, tag))
-    #
-    #             break
 
 
-def semver_bump(tag_latest, dest_branch):
+def semver_bump(tag_latest):
     print("Generate New Version")
-    print("Dest Branch:", dest_branch)
 
-    # if dest_branch == 'development':
-    #     # Commit feature to development branch, MINOR bump
-    #     # Bump Minor position - Example: development @1.2.0-dev --> @1.3.0-dev
-    #
-    #     tag_latest = semver.tag_meta['development'][0]['tag_latest']
-    #     tag_latest_nosuffix = semver.tag_meta['development'][0]['tag_latest_nosuffix']
-    #     tag_suffix = semver.tag_meta['development'][0]['tag_suffix']
-    #
-    #     # Convert semantic version (without suffix) to integers
-    #     MAJOR = int(tag_latest_nosuffix.split(".")[0])
-    #     MINOR = int(tag_latest_nosuffix.split(".")[1])
-    #     PATCH = int(tag_latest_nosuffix.split(".")[2])
-    #
-    #     # Bump version
-    #     MINOR = MINOR + 1
-    #     PATCH = 0  # Reset Patch
-    #
-    #     tag_next = "{0}.{1}.{2}{3}".format(MAJOR, MINOR, PATCH, tag_suffix)
-    #
-    # elif dest_branch == 'master':
-    #     # Retain version, drop suffix. No version bump
-    #     # Example: development @1.2.0-dev --> master @1.2.0
-    #
-    #     tag_latest_nosuffix = semver.tag_meta['development'][0]['tag_latest_nosuffix']
-    #     tag_next = tag_latest_nosuffix
-
-    # Convert semantic version (without suffix) to integers
     MAJOR = int(tag_latest.split(".")[0])
     MINOR = int(tag_latest.split(".")[1])
     PATCH = int(tag_latest.split(".")[2])
@@ -132,15 +85,19 @@ def semver_bump(tag_latest, dest_branch):
     return "{0}.{1}.{2}".format(MAJOR, MINOR, PATCH)
 
 
-def mh_config(mode='live', dest_branch=None):
+def mh_config(mode='live', dest_branch=None, repo_name=None):
     mh_config_model = MhConfigModel()
 
     if mode == 'live':
         print("{0} MODE: Loading config from Github Secrets and ENV Variables".format(mode.upper()))
         mh_config_model.dest_branch = os.environ["GITHUB_REF"].split('/')[2]
         mh_config_model.github_api_token = os.environ["INPUT_REPO-TOKEN"]
+        mh_config_model.repo_name = os.environ["INPUT_REPO-NAME"].split('/')[1]
+        print(mh_config_model.github_api_token)
 
-        print("Destination Branch:", mh_config_model.dest_branch, "API Repo Token: From Github Secrets")
+        print("Repo Name:", mh_config_model.repo_name,
+              "\nDestination Branch:", mh_config_model.dest_branch,
+              "\nAPI Repo Token: From Github Secrets")
 
     elif mode == 'local':
         # Read configs from file and function input parameters
@@ -157,9 +114,11 @@ def mh_config(mode='live', dest_branch=None):
 
         # Set source and destination TEST branches
         mh_config_model.dest_branch = dest_branch
+        mh_config_model.repo_name = repo_name
 
-        print("Destination Branch:", mh_config_model.dest_branch,
-              "API Repo Token: From Github Secrets")
+        print("Repo Name:", mh_config_model.repo_name,
+              "\nDestination Branch:", mh_config_model.dest_branch,
+              "\nAPI Repo Token: From Github Secrets")
 
     return mh_config_model
 
@@ -172,31 +131,36 @@ def push_github_tag(repo_name, dest_branch, tag_next):
 
     # Get latest commit from destination branch (post MERGE)
     git_branch = repo.get_branch(branch=dest_branch)
+    sha = git_branch.commit.sha
+    commit_message = repo.get_git_commit(sha=git_branch.commit.sha).message
+    prerelease = semver.tag_meta[dest_branch][0]['prerelease']
+    prerelease_name = semver.tag_meta[dest_branch][0]['prerelease_name']
 
     # Push next tag to repository
-    prerelease = semver.tag_meta[dest_branch][0]['prerelease']
     print('New tag: {0}\nSHA: {1}\nOn Branch: {2}\nPrerelease: {3}'.format(tag_next,
-                                                                           git_branch.commit.sha,
+                                                                           sha,
                                                                            dest_branch,
                                                                            prerelease))
 
-    repo.create_git_tag_and_release(tag=tag_next,
-                                    tag_message='Test Message',
-                                    release_name='Test Release Name',
-                                    release_message='Test Release Message',
-                                    object=git_branch.commit.sha,
-                                    type='commit',
-                                    prerelease=prerelease)
+    # repo.create_git_tag_and_release(tag=tag_next,
+    #                                 tag_message='Test Message',
+    #                                 release_name=prerelease_name,
+    #                                 release_message=commit_message,
+    #                                 object=sha,
+    #                                 type='commit',
+    #                                 prerelease=prerelease)
+
 
 
 
 
 # ---- Local Testing INPUTS ----
-# Set mode: Local (local config) or Live (GitHub Secrets)
-# mh_config = mh_config(mode='local', dest_branch='master')
+# Set execution mode: Local (local config) or Live (GitHub Secrets)
+# mh_config = mh_config(mode='local', dest_branch='development', repo_name='branching-test')
 mh_config = mh_config(mode='live')
 # ------------------------------
-semver = SemVerModel(repo_name="branching-test",
+
+semver = SemVerModel(repo_name=mh_config.repo_name,
                      dest_branch=mh_config.dest_branch)
 
 # Instantiate GitHub connection object
@@ -209,21 +173,17 @@ tags = get_github_tags(repo_name=semver.repo_name)
 semver.tag_latest = get_latest_tag(git_tags=tags)
 
 # Generate new tag
-semver.tag_next = semver_bump(tag_latest=semver.tag_latest,
-                              dest_branch=semver.dest_branch)
+semver.tag_next = semver_bump(tag_latest=semver.tag_latest)
 
 push_github_tag(repo_name=semver.repo_name,
                 dest_branch=semver.dest_branch,
                 tag_next=semver.tag_next)
 
-# print("::set-output name=tag_new::{0}".format(semver.tag_next))
 
 '''
 TODO:
 - Update release body message
-- Flag for local testing
 - PR checkbox to bump MAJOR, MINOR
-- Hotfix branches
 
 IMPLEMENTATION STEPS
 - Create repo secret REPO-TOKEN
